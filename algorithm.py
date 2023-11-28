7a67d777e4bcb81eb4e73f4577f9cede1b1b9683
TopGun1405 <fstopgun0317@gmail.com>
2023-11-28 15:44:33 +0900

    init

---
+++ algorithm.py
@@ -0,0 +1,18 @@
+def first_come_first_served():
+    pass
+
+
+def round_robin():
+    pass
+
+
+def shortest_job_first():
+    pass
+
+
+def shortest_remaining_time_next():
+    pass
+
+
+def high_response_ratio_next():
+    pass
---
+++ simulator.py
@@ -0,0 +1,18 @@
+class Simulator:
+    def __init__(self) -> None:
+        self.currentState = None
+
+    def changeToFCFS(self) -> None:
+        self.currentState = None
+
+    def changeToRR(self) -> None:
+        pass
+
+    def changToSJF(self) -> None:
+        pass
+
+    def changeToSRTN(self) -> None:
+        pass
+
+    def changeToHRRN(self) -> None:
+        pass
