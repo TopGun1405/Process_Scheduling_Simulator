7fd8798c740b72d30a9fd7a6c760810d55ff1cec
TopGun1405 <fstopgun0317@gmail.com>
2023-11-28 16:54:28 +0900

    Create process.py

---
+++ process.py
@@ -0,0 +1,37 @@
+class Process:
+    def __init__(self, name="Pn", arrival_time=0, burst_time=0) -> None:
+        self.name = name
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        self.waiting_time = 0
+        self.turnAround_time = 0
+        self.normalized_turnAround_time = 0
+
+    def __str__(self) -> str:
+        text = (
+            "[Process Name: {0}] | "
+            "AT: {1}, "
+            "BT: {2}, "
+            "WT: {3}, "
+            "TT: {4}, "
+            "NTT: {5}"
+        ).format(
+            self.name,
+            self.arrival_time,
+            self.burst_time,
+            self.waiting_time,
+            self.turnAround_time,
+            self.normalized_turnAround_time
+        )
+        return text
+
+    def __getitem__(self, key=str):
+        info = {
+            'name': self.name,
+            'AT': self.arrival_time,
+            'BT': self.burst_time,
+            'WT': self.waiting_time,
+            'TT': self.turnAround_time,
+            'NTT': self.normalized_turnAround_time
+        }
+        return info[key]
