948402e1daed92e19621ffe0693e12e82825bc5f
TopGun1405 <fstopgun0317@gmail.com>
2023-11-30 23:02:22 +0900

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,18 +1,34 @@
+from process import Process
+from processor import Processor
+
+
 class Simulator:
     def __init__(self) -> None:
         self.currentState = None
+        self.timeQuantum = 0
+        self.allProcess = []
+        self.allProcessor = [Processor(name="Processor 1")]
 
-    def changeToFCFS(self) -> None:
-        self.currentState = None
+    def __str__(self) -> str:
+        text = "Process: {0}\nProcessor: {1}\n".format(
+            ", ".join(map(str, self.allProcess)), ", ".join(map(str, self.allProcessor))
+        )
+        return text
 
-    def changeToRR(self) -> None:
+    def changeToPcoreFirst(self) -> None:
         pass
 
-    def changToSJF(self) -> None:
+    def changeToEcoreFirst(self) -> None:
         pass
 
-    def changeToSRTN(self) -> None:
-        pass
+    def addProcess(self) -> None:
+        newProcess = Process(
+            name=f"P{len(self.allProcess) + 1}"
+        )
+        self.allProcess.append(newProcess)
 
-    def changeToHRRN(self) -> None:
-        pass
+    def addProcessor(self) -> None:
+        newProcessor = Processor(
+            name=f"Processor {len(self.allProcessor) + 1}"
+        )
+        self.allProcessor.append(newProcessor)
