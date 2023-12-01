a3f6d9858049e50e8d804949b978d7e00b6c2820
TopGun1405 <fstopgun0317@gmail.com>
2023-12-01 21:51:51 +0900

    Update processor.py

--- simulator/processor.py
+++ simulator/processor.py
@@ -1,16 +1,25 @@
+priority = {
+    0: "Ecore",
+    1: "Pcore"
+}
+
+
 class Processor:
     def __init__(self, name="Processor N"):
         self.readyQueue = []
         self.name = name
+        self.priority = priority[0]
 
     def __len__(self) -> int:
         return len(self.readyQueue)
 
     def __str__(self) -> str:
         text = (
-            "[Processor Name: {0}]"
+            "[Processor Name: {0}] | "
+            "Priority: {1}"
         ).format(
-            self.name
+            self.name,
+            self.priority
         )
         return text
 
