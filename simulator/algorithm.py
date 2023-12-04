85bab9635f76b9675ebd69ec78d8f7e705f7753e
TopGun1405 <fstopgun0317@gmail.com>
2023-12-04 17:14:33 +0900

    Update algorithm.py

--- simulator/algorithm.py
+++ simulator/algorithm.py
@@ -1,26 +1,47 @@
-def first_come_first_served():
-    pass
+from processor import Processor
 
 
-def fcfs():
-    first_come_first_served()
+def first_come_first_served(processor: Processor) -> int:
+    turnAround_time = 0
+
+    return turnAround_time
 
 
-def FCFS():
-    first_come_first_served()
+# def FCFS(process: Process):
+#     first_come_first_served(process)
 
 
 def round_robin():
     pass
 
 
+# def RR():
+#     round_robin()
+
+
 def shortest_job_first():
     pass
+
+
+# def SJF():
+#     shortest_job_first()
 
 
 def shortest_remaining_time_next():
     pass
 
 
+# def SRTN():
+#     shortest_remaining_time_next()
+
+
 def high_response_ratio_next():
+    pass
+
+
+# def HRRN():
+#     high_response_ratio_next()
+
+
+if __name__ == "__main__":
     pass
