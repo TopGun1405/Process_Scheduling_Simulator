000371a9f3b40ff0c668692b9d80f0fb4690375b
TopGun1405 <fstopgun0317@gmail.com>
2023-12-09 23:36:54 +0900

    Update process.py

--- simulator/process.py
+++ simulator/process.py
@@ -25,20 +25,32 @@ class Process:
         )
         return text
 
-    def __getitem__(self, key: str) -> any:
+    def __getitem__(self, key: str | int) -> str | int:
         info = {
             'name': self.name,
             'AT': self.arrival_time,
             'BT': self.burst_time,
             'WT': self.waiting_time,
             'TT': self.turnAround_time,
-            'NTT': self.normalized_turnAround_time
+            'NTT': self.normalized_turnAround_time,
+
+            0: self.name,
+            1: self.arrival_time,
+            2: self.burst_time,
+            3: self.waiting_time,
+            4: self.turnAround_time,
+            5: self.normalized_turnAround_time
         }
         try:
             return info[key]
         except KeyError:
             message = (
                 "[KeyError]\n"
-                "Key List: [name, AT, BT, WT, TT, NTT]"
+                "Process Name   : (name or 0)\n"
+                "Arrival Time   : (AT or 1)\n"
+                "Burst Time     : (BT or 2)\n"
+                "Waiting Time   : (WT or 3)\n"
+                "TurnAround Time: (TT or 4)\n"
+                "Normalized TT  : (NTT or 5)"
             )
             return message
