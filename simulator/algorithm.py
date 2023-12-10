6193602fc3bd51f1f0a658f5fda3a7d13c4dff8c
TopGun1405 <fstopgun0317@gmail.com>
2023-12-10 15:43:36 +0900

    Update algorithm.py

--- simulator/algorithm.py
+++ simulator/algorithm.py
@@ -29,46 +29,55 @@ def first_come_first_served(readyQueue: deque[Process]) -> list[Process]:
 
 
 def FCFS(readyQueue: deque[Process]) -> list[Process]:
+    runtime = 0
     endList: list[Process] = first_come_first_served(readyQueue)
     return endList
 
 
 def round_robin(readyQueue: deque[Process], timeQuantum: int) -> list[Process]:
+    runtime = 0
     endList: list[Process] = []
     return endList
 
 
 def RR(readyQueue: deque[Process], timeQuantum: int) -> list[Process]:
+    runtime = 0
     endList: list[Process] = round_robin(readyQueue, timeQuantum)
     return endList
 
 
 def shortest_job_first(readyQueue: deque[Process]) -> list[Process]:
+    runtime = 0
     endList: list[Process] = []
     return endList
 
 
 def SJF(readyQueue: deque[Process]) -> list[Process]:
+    runtime = 0
     endList: list[Process] = shortest_job_first(readyQueue)
     return endList
 
 
 def shortest_remaining_time_next(readyQueue: deque[Process]) -> list[Process]:
+    runtime = 0
     endList: list[Process] = []
     return endList
 
 
 def SRTN(readyQueue: deque[Process]) -> list[Process]:
+    runtime = 0
     endList: list[Process] = shortest_remaining_time_next(readyQueue)
     return endList
 
 
 def high_response_ratio_next(readyQueue: deque[Process]) -> list[Process]:
+    runtime = 0
     endList: list[Process] = []
     return endList
 
 
 def HRRN(readyQueue: deque[Process]) -> list[Process]:
+    runtime = 0
     endList: list[Process] = high_response_ratio_next(readyQueue)
     return endList
 
