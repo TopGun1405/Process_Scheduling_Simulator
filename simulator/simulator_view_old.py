8304b15588075c7a3234581a9a1b9b6a61dfd87c
TopGun1405 <fstopgun0317@gmail.com>
2024-01-02 22:51:16 +0900

    Update simulator_view_old.py

--- simulator/simulator_view_old.py
+++ simulator/simulator_view_old.py
@@ -1,17 +1,28 @@
 import sys
 
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt6.QtWidgets import QLabel, QPushButton
+from PyQt6.QtWidgets import QLabel, QPushButton, QLineEdit
 from PyQt6.QtWidgets import QVBoxLayout, QHBoxLayout
 
 from simulator import Simulator
 
 
-class OldSimulatorView(QWidget):
+class OldSimulatorView(QMainWindow):
     def __init__(self):
         super().__init__()
 
         newSimulator = Simulator()
+
+        mainLayout = QVBoxLayout()
+        centralWidget = QWidget()
+
+        pane = QHBoxLayout()
+        processNameLineEdit = QLineEdit()
+        pane.addWidget(processNameLineEdit)
+
+        mainLayout.addLayout(pane)
+        centralWidget.setLayout(mainLayout)
+        self.setCentralWidget(centralWidget)
 
         self.setWindowTitle("Process Scheduling Simulator")
         self.show()
