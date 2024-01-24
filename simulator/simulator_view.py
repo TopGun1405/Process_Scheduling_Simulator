6cf1f39736cefed73a0e66a5cef3ca6ef8845d67
TopGun1405 <fstopgun0317@gmail.com>
2024-01-24 23:20:15 +0900

    Update simulator_view.py

--- simulator/simulator_view.py
+++ simulator/simulator_view.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt6.QtWidgets import QLabel, QPushButton
+from PyQt6.QtWidgets import QLabel, QPushButton, QLineEdit
 from PyQt6.QtWidgets import QVBoxLayout, QHBoxLayout
 
 from simulator import Simulator
